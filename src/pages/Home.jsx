77281d3638fee07854619242fb869f17edf1444b
happlesful <Swislar1108@gmail.com>
2023-08-30 10:45:04 +0800

    updated home page for navigation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,82 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { SharedStateContext, SharedStateProvider } from "../SharedStateContext";
+import { BiHomeAlt2 } from "react-icons/bi";
+import { FiMenu, FiSettings, FiClock, FiSearch } from "react-icons/fi";
+import { HiOutlineInformationCircle, HiOutlineGlobeAlt } from "react-icons/hi";
+import { LuBrainCircuit } from "react-icons/lu";
+import { MdWorkOutline } from "react-icons/md";
 
 const Home = () => {
+  const {
+    activeSidebar,
+    setActiveSidebar,
+    isClicked,
+    setIsClicked,
+    screenSize,
+    setScreenSize,
+    handleClick,
+  } = useContext(SharedStateContext);
+
   return (
-    <div className="py-5 px-10">
-      <p className="text-lg font-bold underline decoration-2 mb-4">Home</p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad culpa
-      perferendis illum voluptates et ex officia! Corrupti voluptate maxime
-      placeat eveniet mollitia modi pariatur nihil similique ipsa! Repudiandae
-      laudantium laboriosam, rerum cumque perferendis nostrum distinctio in
-      alias nesciunt accusantium quaerat. Soluta quaerat repellendus veniam
-      tempore at, incidunt voluptates ea. Aliquam assumenda dicta, repellendus,
-      amet inventore quaerat veniam autem, fugiat saepe sed deserunt! Tempore
-      perspiciatis voluptas totam quos consequatur enim temporibus sequi non
-      quas! Ex quaerat doloremque repellendus nisi voluptates sequi architecto
-      quae accusamus dicta animi asperiores sint itaque ipsum error ad dolorem
-      incidunt a enim beatae nemo quia, quibusdam excepturi tenetur inventore.
-      Quas doloremque corporis unde facilis nisi quod repellendus enim, voluptas
-      voluptatum dolorem ducimus ipsa cumque modi non et animi voluptates minima
-      voluptatem nihil beatae? Nulla dignissimos sed, voluptates, consequatur
-      veniam alias est minima commodi aliquam, fugit ad laudantium dicta magni
-      illum rerum aperiam accusamus iure placeat ipsa quos? Corporis accusantium
-      ratione voluptate ut. Dolorem ipsam corrupti alias assumenda, rem cumque
-      nemo sit rerum distinctio perferendis numquam error cupiditate veniam cum
-      non, temporibus earum laudantium quibusdam eligendi amet magni at
-      accusamus? Delectus saepe debitis at dolorum quaerat? Corrupti aperiam
-      repellat animi eum ea veritatis ratione veniam illo debitis minima.
-    </div>
+    <>
+      <span
+        className="flex flex-row items-center justify-center p-5
+      text-lg font-bold underline decoration-2"
+      >
+        <BiHomeAlt2 className="m-2" /> Home
+        <BiHomeAlt2 className="m-2" />
+      </span>
+      <div className="flex flex-col box-border items-cente">
+        <div className="flex flew-row items-center justify-center">
+          <span
+            className="flex flex-col items-center justify-center
+           bg-violet-200 rounded-xl w-40 h-40 m-5 
+           transition ease-in-out delay-50 hover:bg-violet-300"
+            onClick={() => {
+              handleClick("inspiration");
+            }}
+          >
+            <LuBrainCircuit className="scale-[5] mx-24 my-12" />
+            <p className="px-20">Inspiration</p>
+          </span>
+          <span
+            className="flex flex-col items-center justify-center
+            bg-violet-200 rounded-xl w-40 h-40 m-5 
+           transition ease-in-out delay-50 hover:bg-violet-300"
+            onClick={() => {
+              handleClick("time");
+            }}
+          >
+            <FiClock className="scale-[5] mx-24 my-12" />
+            <p className="px-20">Clock</p>
+          </span>
+        </div>
+        <div className="flex flew-row items-center justify-center">
+          <span
+            className="flex flex-col items-center justify-center
+            bg-violet-200 rounded-xl w-40 h-40 m-5 
+           transition ease-in-out delay-50 hover:bg-violet-300"
+            onClick={() => {
+              handleClick("productivity");
+            }}
+          >
+            <MdWorkOutline className="scale-[5] mx-24 my-12" />
+            <p className="px-20">Productivity</p>
+          </span>
+          <span
+            className="flex flex-col items-center justify-center
+            bg-violet-200 rounded-xl w-40 h-40 m-5 
+           transition ease-in-out delay-50 hover:bg-violet-300"
+            onClick={() => {
+              handleClick("about");
+            }}
+          >
+            <HiOutlineInformationCircle className="scale-[5] mx-24 my-12" />
+            <p className="px-20">Info</p>
+          </span>
+        </div>
+      </div>
+    </>
   );
 };
 
