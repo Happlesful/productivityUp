6d023d24666178461d73fa82ea7e6c2595d44c95
happlesful <Swislar1108@gmail.com>
2023-08-30 10:46:09 +0800

    added current time to time page

--- src/pages/Time.jsx
+++ src/pages/Time.jsx
@@ -1,31 +1,38 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { SharedStateContext, SharedStateProvider } from "../SharedStateContext";
+import { FiClock } from "react-icons/fi";
 
 const Time = () => {
+  const { time, setTime } = useContext(SharedStateContext);
+
+  useEffect(() => {
+    const timeInterval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timeInterval);
+    };
+  }, []);
+
   return (
-    <div className="py-5 px-10">
-      <p className="text-lg font-bold underline decoration-2 mb-4">Time</p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
-      provident molestiae vel omnis tempora incidunt, distinctio magni unde
-      impedit nam pariatur quidem magnam reprehenderit? Officiis iusto, quaerat
-      ipsum non excepturi omnis quia, commodi similique perferendis consequatur
-      nulla illum vel hic accusantium dolorum distinctio dolorem velit earum
-      placeat culpa maiores rem! Vitae iusto libero ullam omnis architecto neque
-      doloremque, vero ea animi qui. Nostrum pariatur quis et sed optio veniam
-      totam, odit natus necessitatibus, amet suscipit provident, dolores fugiat
-      eaque illum libero facilis beatae. Consectetur suscipit dignissimos
-      ducimus numquam reiciendis modi, cum impedit pariatur quo veniam qui
-      doloremque mollitia facilis at libero laudantium hic sunt exercitationem
-      delectus. Dignissimos, reiciendis nisi dolor vero nobis, voluptates iure
-      itaque, deserunt nesciunt alias architecto. Accusamus distinctio
-      voluptates quo voluptatum iusto asperiores, accusantium provident,
-      molestiae autem doloribus sapiente obcaecati repellendus? Veniam, beatae
-      facere. Vitae, enim quo. Aspernatur, pariatur. Rerum porro expedita vel,
-      aspernatur labore mollitia ad libero recusandae molestiae praesentium
-      tempora reiciendis repellat veniam necessitatibus, maxime quisquam, ullam
-      ab quaerat dolorum magni eveniet. Aperiam accusamus facilis tempora
-      corporis obcaecati dolor laudantium eos illum quidem ipsam. Eveniet, qui
-      est maxime odio quasi dolore illo, ad, a nisi iste alias aut fugit modi
-      ipsa molestiae hic totam velit.
+    <div className="p-5">
+      <span
+        className="text-lg font-bold underline decoration-2 mb-1
+      flex flex-row justify-center items-center"
+      >
+        <FiClock className="m-2" />
+        Current Time
+        <FiClock className="m-2" />
+      </span>
+      <span className="flex justify-center">
+        <p
+          className="text-3xl font-bold font-serif scale-125 
+        bg-gray-200 p-2 rounded-xl"
+        >
+          {time.toLocaleTimeString()}
+        </p>
+      </span>
     </div>
   );
 };
